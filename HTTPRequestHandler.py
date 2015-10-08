a59be2b2f09f318e18bb1115ca0a88941d26f1d1
Daniel Neel <dneelyep@gmail.com>
2015-10-08 15:35:18 -0400

    Break the HTTP server into a separate process from the hub controller process, and add the ability to respond differently depending on the URL the GET request is sent to.
    This way, both the hub controller and HTTP server processes will be running side-by-side. Whenever the HTTP server receives a request, it is parsed, and passed onto the hub controller in the form of a command to be executed.

--- HTTPRequestHandler.py
+++ HTTPRequestHandler.py
@@ -1,19 +1,59 @@
-from http.server import BaseHTTPRequestHandler
+import http.server
 import datetime
+import HubController
 
-class HTTPRequestHandler(BaseHTTPRequestHandler):
+class HTTPRequestHandler(http.server.BaseHTTPRequestHandler):
 
     def do_GET(self):
         try:
-            if self.path.endswith('.html'):
+            print(self.path)
+            if self.path   == "/get_door_status":
+                HubController.PushCommand(HubController.CMD_GET_DOOR_STATUS)
                 self.send_response(200)
-
                 self.send_header('Content-type','text/plain')
                 self.end_headers()
-
                 responseText = "Current time: " + datetime.datetime.strftime(datetime.datetime.now(), '%Y-%m-%d %H:%M:%S')
                 print("Response: " + responseText)
                 self.wfile.write(bytes(responseText, "utf-8"))
                 return
+            elif self.path == "/get_light_status":
+                HubController.PushCommand(HubController.CMD_GET_LIGHT_STATUS)
+                self.send_response(200)
+                self.send_header('Content-type','text/plain')
+                self.end_headers()
+                responseText = "Current time: " + datetime.datetime.strftime(datetime.datetime.now(), '%Y-%m-%d %H:%M:%S')
+                print("Response: " + responseText)
+                self.wfile.write(bytes(responseText, "utf-8"))
+                return
+            elif self.path == "/get_temperature_status":
+                HubController.PushCommand(HubController.CMD_GET_TEMPERATURE_STATUS)
+                self.send_response(200)
+                self.send_header('Content-type','text/plain')
+                self.end_headers()
+                responseText = "Current time: " + datetime.datetime.strftime(datetime.datetime.now(), '%Y-%m-%d %H:%M:%S')
+                print("Response: " + responseText)
+                self.wfile.write(bytes(responseText, "utf-8"))
+                return
+            else:
+                self.send_error("TODO Throw some kind of undefined command error here.")
         except IOError:
             self.send_error(404, 'file not found')
+        # TODO How do I handle other types of errors?
+
+def StartServer():
+    print("Server starting...")
+    #ip and port of server
+    #by default http server port is 80
+    server_address = ('127.0.0.1', 80)
+    httpd = http.server.HTTPServer(server_address, HTTPRequestHandler)
+    print('http server is running...')
+    httpd.serve_forever()
+
+#################################################################################
+##### Starts the HTTP server which accepts commands from the Android phone. #####
+#################################################################################
+def main():
+    StartServer()
+
+if __name__ == "__main__":
+    main()
