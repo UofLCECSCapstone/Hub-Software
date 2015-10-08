2f20a65878e43c011047bd88f74c6c8936b0effe
Daniel Neel <dneelyep@gmail.com>
2015-10-08 15:33:37 -0400

    Rename "test.py" to a sane file name (HubController.py). Add "constants" and a method to push commands onto the hub controller's queue of commands to execute.

--- HubController.py
+++ HubController.py
@@ -1,11 +1,17 @@
 #! python
 
-import http.server
-from HTTPRequestHandler import HTTPRequestHandler
-
 # TODO Create classes that represent each type of peripheral - Lightbulb, Thermostat, Door -
 #      and populate objects of those classes in the below methods, to keep things logically separated out.
 #      Procedural spaghetti is bad.
+# TODO This should probably be converted into a class.
+
+#################################
+##### Command abbreviations #####
+#################################
+CMD_GET_DOOR_STATUS = "GetDoorStatus"
+CMD_GET_LIGHT_STATUS = "GetLightStatus"
+CMD_GET_TEMPERATURE_STATUS = "GetTemperatureStatus"
+
 ###################
 ##### Methods #####
 ###################
@@ -22,57 +28,58 @@ def UpdateDoorStatus():
     """
     Checks each door connected to the hub, to see if it is opened or closed.
     """
-    print("Checking door 1 status...")
-    print("Checking door 2 status...")
-    print("...")
-    print("Checking door n status...")
+    # print("Checking door 1 status...")
+    # print("Checking door 2 status...")
+    # print("...")
+    # print("Checking door n status...")
 
 def UpdateThermostatStatus():
     """
     Checks the current temperature that the thermostat is set at.
     """
-    print("Checking thermostat temperature...")
+    # print("Checking thermostat temperature...")
 
 def UpdateLightStatus():
     """
     Checks each lightbulb connected to the hub, to see if it is turned on or off.
     """
-    print("Checking lightbulb 1 status...")
-    print("Checking lightbulb 2 status...")
-    print("...")
-    print("Checking lightbulb n status...")
+    # print("Checking lightbulb 1 status...")
+    # print("Checking lightbulb 2 status...")
+    # print("...")
+    # print("Checking lightbulb n status...")
 
 def CheckForCommands():
     """
     Checks to see if the user has requested that a command be performed.
     If so, the command is performed.
     """
-    print("Checking for commands from the user...")
-    print("TODO Fill this in with actual checks.")
+    # print("Checking for commands from the user...")
+    # print("TODO Fill this in with actual checks.")
+
+def PushCommand(commandAbbreviation):
+    """
+    Pushes a command to the hub controller's queue of commands to be processed.
+    :param commandAbbreviation: An abbreviation representing the command to execute.
+    """
+    if commandAbbreviation == CMD_GET_DOOR_STATUS:
+        print("Getting door status...")
+    elif commandAbbreviation == CMD_GET_LIGHT_STATUS:
+        print("Getting light status...")
+    elif commandAbbreviation == CMD_GET_TEMPERATURE_STATUS:
+        print("Getting temperature status...")
+    else:
+        print("TODO Handle this case somehow.")
 
 #######################
 ##### Main method #####
 #######################
 def main():
-    StartServer()
     try:
         while True:
             UpdateDeviceStatus()
             CheckForCommands()
     except:
         print("TODO Handle errors here appropriately. Exit the program?")
-
-#################################################################################
-##### Starts the HTTP server which accepts commands from the Android phone. #####
-#################################################################################
-def StartServer():
-    print("Server starting...")
-    #ip and port of server
-    #by default http server port is 80
-    server_address = ('127.0.0.1', 80)
-    httpd = http.server.HTTPServer(server_address, HTTPRequestHandler)
-    print('http server is running...')
-    httpd.serve_forever()
 
 if __name__ == "__main__":
     main()
